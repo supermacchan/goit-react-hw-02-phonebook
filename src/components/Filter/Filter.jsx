510060fcee51e60dc3e70c3511d5621f528c4781
Olga Birkovska <birkovskaya92@ukr.net>
2022-12-01 14:10:03 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,20 @@
-import { Component } from "react";
 import PropTypes from 'prop-types';
 import css from './Filter.module.css';
 
-export class Filter extends Component {
-    static propTypes = {
-        value: PropTypes.string.isRequired,
-        onChange: PropTypes.func.isRequired,
-    };
-
-    render() {
-        return (
-            <label>
-                <input
+export const Filter = ({value, onChange}) => {
+    return (
+        <label>
+            <input
                     type="text"
-                    value={this.props.value}
-                    onChange={this.props.onChange}
+                    value={value}
+                    onChange={onChange}
                     className={css.filterInput}
-                />
-            </label>
-        );
-    };
-}
+            />
+        </label>
+    );
+};
+
+Filter.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+};
